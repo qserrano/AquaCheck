ec3934bf86962632b6b6b2eff26e62e17b5dbc48
cartrotech <cartrotech@gmail.com>
2025-06-11 10:56:42 +0200

    feat: Remove succees.html and replace with options.html

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
+app.get('/options', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'options.html'));
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
